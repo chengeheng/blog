0ece6b2f859fb427a1689456bf2f86acfc327e5b
chengeheng <465819676@qq.com>
2019-09-29 17:20:37 +0800

    添加loading组件

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,20 +3,27 @@ import styles from "./index.module.less";
 import Info from "./components/info";
 import LatestNote from "./components/latestNote";
 import CopyRight from "./components/copyRight";
-// import A from "cgh-ui/components/test";
+import Loading from "cgh-ui/components/Loading";
 const HomePage = () => {
 	return (
 		<div className={styles.main}>
-			<div className={styles.left}>
-				<div className={styles.head_portrait} />
-				<div className={styles.introduce}></div>
-				{/* <A></A> */}
-			</div>
-			<div className={styles.introduction}>
-				<Info className={styles.introduce_info} />
-				<LatestNote className={styles.introduce_note} />
-				<CopyRight className={styles.introduce_copyright} />
-			</div>
+			<Loading loading={true}>
+				<div
+					className={styles.left}
+					onClick={() => {
+						console.log("aaa");
+					}}
+				>
+					<div className={styles.head_portrait} />
+					<div className={styles.introduce}></div>
+					{/* <A></A> */}
+				</div>
+				<div className={styles.introduction}>
+					<Info className={styles.introduce_info} />
+					<LatestNote className={styles.introduce_note} />
+					<CopyRight className={styles.introduce_copyright} />
+				</div>
+			</Loading>
 		</div>
 	);
 };
