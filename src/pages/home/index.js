e723d007df6a9048781985bed506c54ed8a14c4b
chengeheng <465819676@qq.com>
2019-09-30 14:11:50 +0800

    little change

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,31 +1,30 @@
 import React from "react";
 import styles from "./index.module.less";
+import { withRouter } from "react-router-dom";
 import Info from "./components/info";
 import LatestNote from "./components/latestNote";
 import CopyRight from "./components/copyRight";
-import Loading from "cgh-ui/components/Loading";
-const HomePage = () => {
+import { ReactComponent as NextSvg } from "images/home/next.svg";
+const HomePage = props => {
+	const { history } = props;
+	const jumpToNote = () => {
+		history.push("/note");
+	};
 	return (
 		<div className={styles.main}>
-			<Loading loading={true}>
-				<div
-					className={styles.left}
-					onClick={() => {
-						console.log("aaa");
-					}}
-				>
-					<div className={styles.head_portrait} />
-					<div className={styles.introduce}></div>
-					{/* <A></A> */}
-				</div>
-				<div className={styles.introduction}>
-					<Info className={styles.introduce_info} />
-					<LatestNote className={styles.introduce_note} />
-					<CopyRight className={styles.introduce_copyright} />
-				</div>
-			</Loading>
+			<div className={styles.left}>
+				<div className={styles.head_portrait} />
+				<div className={styles.introduce}></div>
+				{/* <A></A> */}
+			</div>
+			<div className={styles.introduction}>
+				<Info className={styles.introduce_info} />
+				<LatestNote className={styles.introduce_note} />
+				<CopyRight className={styles.introduce_copyright} />
+			</div>
+			<NextSvg className={styles.next} onClick={jumpToNote} />
 		</div>
 	);
 };
 
-export default HomePage;
+export default withRouter(HomePage);
