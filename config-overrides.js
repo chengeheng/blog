343a2ab88a07ee4706ca7493e820eb0feb780ad4
chengeheng <465819676@qq.com>
2019-10-28 22:43:37 +0800

    修改打包配置

--- config-overrides.js
+++ config-overrides.js
@@ -3,9 +3,93 @@ const {
 	fixBabelImports,
 	addLessLoader,
 	babelInclude,
+	addDecoratorsLegacy,
 	addWebpackResolve
 } = require("customize-cra");
 const path = require("path");
+
+const addCustomize = () => config => {
+	if (process.env.NODE_ENV === "production") {
+		config.devtool = false; //去掉map文件
+		config.module.rules.push({
+			loader: "webpack-ant-icon-loader",
+			enforce: "pre",
+			include: [require.resolve("@ant-design/icons/lib/dist")]
+		});
+		const splitChunksConfig = config.optimization.splitChunks;
+
+		// if (config.entry && config.entry instanceof Array) {
+		// 	config.entry = {
+		// 		main: config.entry,
+		// 		vendor: [
+		// 			"react",
+		// 			"react-dom",
+		// 			"react-router-dom",
+		// 			"react-redux",
+		// 			"redux",
+		// 			"lodash",
+		// 			"moment",
+		// 			"react-router"
+		// 		]
+		// 	};
+		// } else if (config.entry && typeof config.entry === "object") {
+		// 	config.entry.vendor = [
+		// 		"react",
+		// 		"react-dom",
+		// 		"react-router-dom",
+		// 		"react-redux",
+		// 		"redux",
+		// 		"lodash",
+		// 		"moment",
+		// 		"react-router"
+		// 	];
+		// }
+
+		Object.assign(splitChunksConfig, {
+			// chunks: "all",
+			cacheGroups: {
+				reactBase: {
+					test: module => {
+						return /react|redux|prop-types/.test(module.context);
+					}, // 直接使用 test 来做路径匹配，抽离react相关代码
+					chunks: "initial",
+					name: "reactBase",
+					priority: 10
+				},
+				antd: {
+					test: module => /antd/.test(module.context),
+					chunks: "initial",
+					priority: 9,
+					reuseExistingChunk: false,
+					name: "antd"
+				},
+				marked: {
+					test: module => {
+						return /marked/.test(module.context);
+					}, // 直接使用 test 来做路径匹配，抽离react相关代码
+					chunks: "initial",
+					name: "marked",
+					reuseExistingChunk: true,
+					priority: 8
+				},
+				vender: {
+					test: /[\\/]node_modules[\\/]/,
+					name: "vendor",
+					chunks: "initial",
+					reuseExistingChunk: true,
+					priority: 5
+				},
+				common: {
+					name: "common",
+					minSize: 1,
+					reuseExistingChunk: true,
+					priority: 0
+				}
+			}
+		});
+	}
+	return config;
+};
 
 module.exports = override(
 	// 参考https://www.jianshu.com/p/e1dbb940968d
@@ -21,6 +105,7 @@ module.exports = override(
 		javascriptEnabled: true
 	}),
 	babelInclude([path.resolve("src"), path.resolve("node_modules/cgh-ui")]),
+	addDecoratorsLegacy(),
 	addWebpackResolve({
 		extensions: [".js", ".jsx", ".json", ".css", ".less", ".jpg", ".png"],
 		alias: {
@@ -28,5 +113,6 @@ module.exports = override(
 			style: path.resolve(__dirname, "src/style"),
 			images: path.resolve(__dirname, "src/images")
 		}
-	})
+	}),
+	addCustomize()
 );
