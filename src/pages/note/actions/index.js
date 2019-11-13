9181aae72065b668f72699d70772bab7ee5fa7ea
chengeheng <465819676@qq.com>
2019-11-13 10:01:55 +0800

    更改移动端样式

--- src/pages/note/actions/index.js
+++ src/pages/note/actions/index.js
@@ -1,2 +1,29 @@
-export * from "./Main";
-export * from "./Detail";
+import cghFetch from "utils/request";
+export const getMdNote = (stateId, id) => () =>
+	cghFetch(stateId, {
+		url: `/notes/detail/get?id=${id}`,
+		method: "GET"
+	});
+export const getNoteDetail = (stateId, id) => () =>
+	cghFetch(stateId, {
+		url: `/notes/get?id=${id}`,
+		method: "GET"
+	});
+export const getNote = stateId => () =>
+	cghFetch(stateId, {
+		url: "/notes/list",
+		method: "GET"
+	});
+
+export const addNote = value => () =>
+	cghFetch(null, {
+		url: "/notes/add",
+		method: "POST",
+		body: JSON.stringify(value)
+	});
+
+export const getNoteList = stateId => () =>
+	cghFetch(stateId, {
+		url: "/notes/listOrderByYear",
+		method: "GET"
+	});
--- src/pages/note/noteDetail.js
+++ src/pages/note/noteDetail.js
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import styles from "./noteDetail.module.less";
 import { useSelector, useDispatch } from "react-redux";
-import { getMdNote } from "./actions";
+import { getMdNote, getNoteDetail } from "./actions";
 import marked from "marked";
 import hljs from "highlight.js";
-import { Spin } from "antd";
-import { ReactComponent as PreviousSVG } from "images/note/previous.svg";
+import Loading from "cgh-ui/components/Loading";
+import moment from "moment";
 import { getUrlParamHash } from "utils/utils";
 marked.setOptions({
 	renderer: new marked.Renderer(),
@@ -21,43 +21,38 @@ marked.setOptions({
 		return hljs.highlightAuto(code).value;
 	}
 });
-const DETAIL_NOTE = "DETAIL_NOTE";
-
+const NOTE_DETAIL_MD = "NOTE_DETAIL_MD";
+const NOTE_DETAIL = "NOTE_DETAIL";
 const NoteDetail = props => {
 	const dispatch = useDispatch();
-	const { history } = props;
 	const { id } = getUrlParamHash();
 	const localState = useSelector(state => ({
-		data: state.data[DETAIL_NOTE] ? state.data[DETAIL_NOTE] : "",
-		getting: !state.data[DETAIL_NOTE]
+		data: state.data[NOTE_DETAIL_MD] ? state.data[NOTE_DETAIL_MD] : "",
+		detail: state.data[NOTE_DETAIL] ? state.data[NOTE_DETAIL] : "",
+		getting: !state.data[NOTE_DETAIL_MD]
 	}));
-	const { data, getting } = localState;
+	const { data, getting, detail } = localState;
 	useEffect(() => {
-		dispatch(getMdNote(DETAIL_NOTE, id));
+		dispatch(getNoteDetail(NOTE_DETAIL, id));
+		dispatch(getMdNote(NOTE_DETAIL_MD, id));
 	}, [dispatch, id, props]);
-
+	const { name, createTime } = detail;
+	const showTime = moment(createTime).format("MMM DD YYYY");
 	return (
 		<div className={styles.main}>
-			<div className={styles.toolBar}>
-				<PreviousSVG
-					className={styles.previous}
-					onClick={() => {
-						// history.goBack();
-						history.push("/note");
-					}}
-				>
-					返回
-				</PreviousSVG>
+			<div className={styles.title}>
+				<div className={styles.title_name}>{name}</div>
+				<div className={styles.title_date}>{showTime}</div>
 			</div>
 			<div className={styles.body}>
-				<Spin spinning={getting}>
+				<Loading loading={getting}>
 					<div
 						className="content-md"
 						dangerouslySetInnerHTML={{
 							__html: marked(data)
 						}}
 					/>
-				</Spin>
+				</Loading>
 			</div>
 		</div>
 	);
