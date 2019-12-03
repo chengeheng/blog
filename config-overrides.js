5bd80385104027de28409a37d21b7f001e8bc0b8
chengeheng <465819676@qq.com>
2019-12-03 14:14:58 +0800

    删除无用的依赖

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,5 @@
 const {
 	override,
-	fixBabelImports,
 	addLessLoader,
 	babelInclude,
 	addDecoratorsLegacy,
@@ -18,51 +17,9 @@ const addCustomize = () => config => {
 		});
 		let splitChunksConfig = config.optimization.splitChunks;
 
-		// if (config.entry && config.entry instanceof Array) {
-		// 	config.entry = {
-		// 		main: config.entry,
-		// 		vendor: [
-		// 			"react",
-		// 			"react-dom",
-		// 			"react-router-dom",
-		// 			"react-redux",
-		// 			"redux",
-		// 			"lodash",
-		// 			"moment",
-		// 			"react-router"
-		// 		]
-		// 	};
-		// } else if (config.entry && typeof config.entry === "object") {
-		// 	config.entry.vendor = [
-		// 		"react",
-		// 		"react-dom",
-		// 		"react-router-dom",
-		// 		"react-redux",
-		// 		"redux",
-		// 		"lodash",
-		// 		"moment",
-		// 		"react-router"
-		// 	];
-		// }
-
 		Object.assign(splitChunksConfig, {
 			chunks: "all",
 			cacheGroups: {
-				// reactBase: {
-				// 	test: module => {
-				// 		return /react|redux|prop-types/.test(module.context);
-				// 	}, // 直接使用 test 来做路径匹配，抽离react相关代码
-				// 	chunks: "initial",
-				// 	name: "reactBase",
-				// 	priority: -4
-				// },
-				// antd: {
-				// 	test: module => /antd/.test(module.context),
-				// 	chunks: "initial",
-				// 	reuseExistingChunk: false,
-				// 	name: "antd",
-				// 	priority: -10
-				// },
 				marked: {
 					test: module => {
 						return /highlight/.test(module.context);
@@ -72,19 +29,6 @@ const addCustomize = () => config => {
 					reuseExistingChunk: true,
 					priority: -16
 				}
-				// vender: {
-				// 	test: /[\\/]node_modules[\\/]/,
-				// 	name: "vendor",
-				// 	chunks: "initial",
-				// 	reuseExistingChunk: true,
-				// 	priority: -18
-				// },
-				// common: {
-				// 	name: "common",
-				// 	minSize: 1,
-				// 	reuseExistingChunk: true,
-				// 	priority: -20
-				// }
 			}
 		});
 	}
@@ -92,16 +36,7 @@ const addCustomize = () => config => {
 };
 
 module.exports = override(
-	// 参考https://www.jianshu.com/p/e1dbb940968d
-	fixBabelImports("antd", {
-		libraryName: "antd",
-		libraryDirectory: "lib"
-		// style: true
-	}),
 	addLessLoader({
-		// modifyVars: {
-		// 	"@primary-color": "#F5A623"
-		// },
 		javascriptEnabled: true
 	}),
 	babelInclude([path.resolve("src"), path.resolve("node_modules/cgh-ui")]),
