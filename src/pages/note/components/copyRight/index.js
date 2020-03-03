2a063e47ba57319853a8f4b19d4a3d312e47e2f1
chengeheng <465819676@qq.com>
2020-03-03 21:40:39 +0800

    添加工信部备案号

--- src/pages/note/components/copyRight/index.js
+++ src/pages/note/components/copyRight/index.js
@@ -5,8 +5,20 @@ const CopyRight = props => {
 	const { className } = props;
 	return (
 		<div className={`${className} ${styles.main}`}>
-			<div className={styles.top}> Copyright©2019</div>
-			<div className={styles.bottm}> Powered by Geheng Chen</div>
+			<div className={styles.top}>
+				{" "}
+				Copyright©2019 Powered by Geheng Chen
+			</div>
+			<div className={styles.bottom}>
+				工信部备案：
+				<a
+					rel="noopener noreferrer"
+					href="http://www.beian.miit.gov.cn"
+					target="_blank"
+				>
+					苏ICP备19053625号
+				</a>
+			</div>
 		</div>
 	);
 };
